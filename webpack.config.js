c4f778c1dd8a99f4c64fcaa17e8d7f0daa12919c
AI1411 <goethe0719@gmail.com>
2020-12-24 00:18:40 +0900

    react導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/index.tsx',
     module: {
         rules: [
             {
-                test: /\.ts$/,
+                test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
         ],
     },
     resolve: {
-        extensions: ['.ts'],
+        extensions: ['.ts','.js','.tsx'],
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "index.ts",
+        filename: "index.tsx",
         publicPath: "dist/",
     }
 }
