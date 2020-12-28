b35fe0d514a1f2a53a2c91841e162196d64d824b
AI1411 <goethe0719@gmail.com>
2020-12-28 23:42:37 +0900

    ページング実装

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -1,9 +1,18 @@
 import Dexie from "dexie";
+import {memo} from "react";
 
 export interface MemoRecord {
     datetime: string
     title: string
     text: string
+}
+
+const NUM_PER_PAGE: number = 10;
+
+export const getMemoPageCount = async (): Promise<number> => {
+    const totalCount = await memos.count();
+    const pageCount = Math.ceil(totalCount / NUM_PER_PAGE);
+    return pageCount > 0 ? pageCount : 1;
 }
 
 const database = new Dexie('markdown-editor');
@@ -14,8 +23,11 @@ export const putMemo = async (title: string, text: string): Promise<void> => {
     const datetime = new Date().toISOString();
     await memos.put({datetime, title, text})
 }
-export const getMemos = ():Promise<MemoRecord[]> => {
+export const getMemos = (page:number):Promise<MemoRecord[]> => {
+    const offset = (page - 1) * NUM_PER_PAGE;
     return memos.orderBy('datetime')
         .reverse()
-        .toArray()
+        .offset(offset)
+        .limit(NUM_PER_PAGE)
+        .toArray();
 }
