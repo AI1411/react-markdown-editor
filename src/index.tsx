f1983772008633957827988302503c89e0acd962
AI1411 <goethe0719@gmail.com>
2020-12-28 11:52:53 +0900

    editorとreact-router実装

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import {render} from "react-dom";
 import styled from "styled-components";
 import {createGlobalStyle} from "styled-components";
 import {Editor} from './pages/editor';
+import {History} from "./pages/history";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle`
     body * {
@@ -12,8 +19,16 @@ const GlobalStyle = createGlobalStyle`
 
 const Main = (
     <>
-        <GlobalStyle />
-        <Editor />
+        <GlobalStyle/>
+        <Router>
+            <Route exact path="/editor">
+                <Editor/>
+            </Route>
+            <Route exact path="/history">
+                <History />
+            </Route>
+            <Redirect to="/editor" path="*"/>
+        </Router>
     </>
 )
 
--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -2,12 +2,19 @@ import * as React from 'react'
 import styled from 'styled-components'
 import {useStateWithStorage} from '../hooks/use_state_with_storage';
 import * as ReactMarkdown from 'react-markdown';
+import {Button} from "../components/button";
+import {putMemo} from "../indexeddb/memos";
+import {SaveModal} from "../components/save_modal";
+import {Link} from "react-router-dom";
 
 const {useState} = React;
 
 const Header = styled.header`
+  align-content: center;
+  display: flex;
   font-size: 1.5rem;
   height: 2rem;
+  justify-content: space-between;
   left: 0;
   line-height: 2rem;
   padding: 0.5rem 1rem;
@@ -15,6 +22,12 @@ const Header = styled.header`
   right: 0;
   top: 0;
 `
+
+const HeaderControl = styled.div`
+    height: 2rem;
+    display: flex;
+    align-content: center;
+  `
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -51,20 +64,38 @@ const StorageKey = 'pages/editor:text';
 
 export const Editor: React.FC = () => {
     const [text, setText] = useStateWithStorage('', StorageKey)
+    const [showModal, setShowModal] = useState(false);
     return (
         <>
             <Header>
                 Markdown Editor
+                <HeaderControl>
+                    <Button onClick={() => setShowModal(true)}>
+                        保存する
+                    </Button>
+                    <Link to="/history">
+                        履歴を見る
+                    </Link>
+                </HeaderControl>
             </Header>
             <Wrapper>
                 <TextArea
                     onChange={(event) => setText(event.target.value)}
                     value={text}
-                    />
+                />
                 <Preview>
                     <ReactMarkdown source={text}/>
                 </Preview>
             </Wrapper>
+            {showModal && (
+                <SaveModal
+                    onSave={(title: string): void => {
+                        putMemo(title, text)
+                        setShowModal(false)
+                    }}
+                    onCancel={() => setShowModal(false)}
+                />
+            )}
         </>
-    )
+    );
 }
